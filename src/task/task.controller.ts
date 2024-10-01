e479c9cbe81620a24bb1b36ebf59bee158b6d9fa
luc-neivor <lucasc@neivor.com>
2024-10-01 19:58:09 -0400

    ADD task controller

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,8 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
 import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskEntity } from './task.entity';
 
 @Controller('task')
 export class TaskController {
 
     constructor(private readonly taskService: TaskService) { }
+
+    @Get()
+    async findOne(@Query('project_id', ParseIntPipe) id: number) {
+        return this.taskService.findOne(id);
+    }
+
+    @Post()
+    async create(@Body() task: CreateTaskDto): Promise<TaskEntity> {
+        return this.taskService.create(task);
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: number, @Body() task: CreateTaskDto) {
+        return this.taskService.update(id, task);
+    }
 }
